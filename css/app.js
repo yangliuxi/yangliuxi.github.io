54fe187cce2c1130682de0cd75ec58130862b962
ILucy <191216298@qq.com>
2016-04-22 19:15:45 +0800

    Site updated: 2016-04-22 19:15:44

--- css/app.js
+++ css/app.js
@@ -1,2 +1,2 @@
 var b=document.getElementsByClassName('post-content');
-b[0].innerHTML=b[0].innerHTML.replace(/<br><br>.*<br>/g,'<br><br>');
+b[0].innerHTML=b[0].innerHTML.replace(/<br><br>.*<br>/g,'');
